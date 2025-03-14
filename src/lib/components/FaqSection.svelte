206e65008eb72e326bcbbf53654247d559fcdf75
Erik <48137656+Quasiich@users.noreply.github.com>
2025-03-15 00:58:39 +0100

    started faq-section & faq-item

---
+++ src/lib/components/FaqSection.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { FaqItem, Button } from "$components";
+</script>
+
+<section class="faq-section">
+  <h2 class="mb-l">Frequently asked questions</h2>
+  <div class="faq-container">
+    <FaqItem />
+  </div>
+  <div class="addtional-info">
+    <Button class="mt-s">Buy now!</Button>
+    <p class="mt-m">Any other questions?</p>
+    <a href="https://github.com/Quasiich">
+      <p>hit me up ♥</p>
+    </a>
+  </div>
+</section>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
-  import { AuthorSection, HeroSection } from "$components";
+  import { AuthorSection, HeroSection, FaqSection } from "$components";
   import { ChapterPreview } from "$components";
 </script>
 
 <HeroSection />
 <ChapterPreview />
 <AuthorSection />
+<FaqSection />
