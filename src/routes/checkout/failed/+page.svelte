f66a6eb86ace867999d17882ec1f96ecf199928b
Erik <rkrettschlag@gmail.com>
2025-03-17 23:03:54 +0100

    stripe payment integration finished

---
+++ src/routes/checkout/failed/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {HeroSection, Button} from "$components";
+</script>
+
+<HeroSection> 
+    <h1>ERROR WHILE</h1>
+    <h1 class="mb-l">PURCHASING</h1>
+    <p class="light-grey mb-s">
+        Unfortunately, there was an issue processing your eBook purchase. We were unable to complete the transaction, and the purchase was not successful. Please review your payment details and try again.
+    </p>
+    <p class="light-grey mb-s">
+        If you believe this was an error, please double-check your payment information and ensure that all fields are correctly filled in. If the problem persists, feel free to reach out to our support team for assistance.
+    </p>
+    <p class="light-grey mb-m">
+        To complete your purchase, please click the checkout button below to try again. Our team is available to assist you should you encounter any further issues. Thank you for your understanding!
+    </p>
+    <Button>Try again</Button>
+</HeroSection>
---
+++ src/routes/checkout/success/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {HeroSection} from "$components";
+</script>
+
+<HeroSection> 
+    <h1>THANK YOU FOR</h1>
+    <h1 class="mb-l">PURCHASING</h1>
+    <p class="light-grey mb-s">
+        Thank you for your purchase! We are pleased to inform you that your eBook has been successfully purchased. You can now download it and enjoy reading it on your preferred e-reader or app at any time.
+    </p>
+    <p class="light-grey mb-s">
+        To download your eBook, simply click on the link provided in the confirmation email sent to your registered email address. If you encounter any issues with the download, please don’t hesitate to reach out for assistance.
+    </p>
+    <p class="light-grey">
+        If you have any questions about your purchase or need help with your eBook, feel free to contact us. Our customer support team is here to ensure you have the best reading experience. Thank you for your trust!
+    </p>
+</HeroSection>
